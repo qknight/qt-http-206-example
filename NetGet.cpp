fdc0a21c8e15aaffab93b0f3ad47a2c41d2b9481
Joachim Schiele <js@lastlog.de>
2013-09-02 19:03:15 +0200

    fixes the cancel behavour when using the c shortcut

--- NetGet.cpp
+++ NetGet.cpp
@@ -6,7 +6,7 @@
 
 NetGet::NetGet(QObject* parent) : QObject(parent) {
     manager = new QNetworkAccessManager(this);
-        m_createRequests=true;
+    m_createRequests=true;
     connect(manager, SIGNAL(finished(QNetworkReply*)),
             this, SLOT(replyFinished(QNetworkReply*)));
 
@@ -16,34 +16,37 @@ NetGet::NetGet(QObject* parent) : QObject(parent) {
 }
 
 void NetGet::printShortcuts() {
-     qDebug() << "'q', then RETURN or CTRL+C to exit immediately; 'c' then RETURN to cancel further downloads";
+    qDebug() << "'q', then RETURN or CTRL+C to exit immediately; 'c' then RETURN to cancel further downloads";
 }
 
 void NetGet::createRequestSlot() {
-    // this example implements a partial download 206 (http) get and prints the result - 1024 bytes - to a shell using hex digits
-    QNetworkRequest rq(QUrl("http://download.qt-project.org/official_releases/online_installers/qt-linux-opensource-1.4.0-x86_64-online.run"));
-    long b = qrand()%10000;
-    QString s = QString("bytes=1-%1").arg(b);
-    rq.setRawHeader("Range", s.toUtf8());
-    rq.setRawHeader("Accept:", "*/*");
-
-    qDebug() << "starting download with " << b << " bytes";
-    QNetworkReply* rp = manager->get(rq);
-    requests.append(rp);
-
     if (m_createRequests) {
+        // this example implements a partial download 206 (http) get and prints the result - 1024 bytes - to a shell using hex digits
+        QNetworkRequest rq(QUrl("http://download.qt-project.org/official_releases/online_installers/qt-linux-opensource-1.4.0-x86_64-online.run"));
+        long b = qrand()%10000;
+        QString s = QString("bytes=1-%1").arg(b);
+        rq.setRawHeader("Range", s.toUtf8());
+        rq.setRawHeader("Accept:", "*/*");
+
+        qDebug() << "starting download with " << b << " bytes";
+        QNetworkReply* rp = manager->get(rq);
+        requests.append(rp);
+
         int ms = qrand() % 5000;
-	qDebug() << "sleeping for " << ms << " ms";
-        QTimer::singleShot(ms, this, SLOT(createRequestSlot()));      
+        qDebug() << "sleeping for " << ms << " ms";
+        QTimer::singleShot(ms, this, SLOT(createRequestSlot()));
     }
 }
 
 void NetGet::cancelDownload() {
+    m_createRequests=false;
     qDebug() << "NetGet::cancelDownload() called";
+    int i = 0;
     foreach(QNetworkReply* rp, requests) {
         rp->abort();
+        qDebug() << "canceling request " << i++;
     }
-    m_createRequests=false;
+    printShortcuts();
 }
 
 
